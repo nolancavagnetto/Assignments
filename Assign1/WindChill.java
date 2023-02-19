e712a94834d83a784e7242541960258d246e1875
nolancavagnetto <108772829+nolancavagnetto@users.noreply.github.com>
2023-02-19 15:05:20 -0500

    Update WindChill.java

--- Assign1/WindChill.java
+++ Assign1/WindChill.java
@@ -24,7 +24,7 @@ public class WindChill {
         }
 
         double T = Double.parseDouble(args[0]);
-        double V = Double.parseDouble(args[1]);
+        double V = Double.parseDouble(args[1]); 
 
         double W = 35.74 + 0.6215*T + (0.4275*T - 35.75)*Math.pow(V, 0.16);
         String w1 = String.format("%.2f", W);
